995f6a0d951c8db6ed6f6a1619aa1c24e0f9e207
Hubert Stärk <hubert.staerk@gmail.com>
2021-04-18 21:00:24 +0200

    :construction: add gird layout home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,82 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="header">Header</div>
+      <div class="left-sidebar">
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+        amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+        nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+        sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+        rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+        ipsum dolor sit amet.
+      </div>
+      <div class="content-1">
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+        amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+        nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+        sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+        rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+        ipsum dolor sit amet.
+      </div>
+      <div class="content-2">
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+        eirmod tempor
+      </div>
+      <div class="right-sidebar">Right Sidebar</div>
+      <div class="footer">Footer</div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: {},
+};
 </script>
+
+<style scoped lang="css">
+.container {
+  height: 100vh;
+  display: grid;
+  grid-template-columns: 1fr 2fr 2fr 1fr;
+  grid-template-rows: auto 1fr auto;
+  grid-template-areas:
+    "header header header header"
+    "leftSidebar content1 content2 rightSidebar"
+    "footer footer footer footer";
+}
+
+.container > div {
+  font-size: 2em;
+}
+
+.header {
+  grid-area: header;
+}
+.left-sidebar {
+  grid-area: leftSidebar;
+  overflow: auto;
+}
+.right-sidebar {
+  grid-area: rightSidebar;
+  overflow: auto;
+}
+.content-1 {
+  grid-area: content1;
+  overflow: auto;
+}
+.content-2 {
+  grid-area: content2;
+  overflow: auto;
+}
+.footer {
+  grid-area: footer;
+}
+</style>
